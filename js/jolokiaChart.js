cdeeb973ba50a9705455777246f1dc63c96d77cf
dovie <4614730+dovie@users.noreply.github.com>
2018-04-11 21:23:00 +0100

    Add Support for multiseries with bulk requests.

--- js/jolokiaChart.js
+++ js/jolokiaChart.js
@@ -13,38 +13,74 @@
  * 
  * */
 function jolokiaChart(url, interval, renderTo, seriesType, seriesLength, mbean, attrName, path) {
-var jolokia = new Jolokia(url);
+    var request={type: "read", mbean: mbean, attribute:attrName};
+    if(path) request.path=path;
+    var title=attrName;
+    var requests=[request];
+    jolokiaMultiSeriesChart(title, url, interval, renderTo, seriesType, seriesLength, requests);
+}
 
-return new Highcharts.Chart({
- chart: {
-  renderTo: renderTo,
-  defaultSeriesType: seriesType,
-  events: {
-   load: function() {
-	var series = this.series[0];
-	setInterval(function() {
-	 var x = (new Date()).getTime();
-	 var attribute = jolokia.getAttribute(mbean, attrName, path);
-	 series.addPoint({
-	  x: new Date().getTime(), //x Axis
-	  y: parseInt(attribute)  //y Axis
-	 }, true, series.data.length >= seriesLength);
-	}, interval);
-   }
+/* Function that wraps new chart creation but supports multiple series 
+ * in the same chart
+ * 
+ * @example
+ * jolokiaMultiSeriesChart("Memory Level", "/jolokia", 5000, "myChart", "spline", 30, [
+ *  { type: "read", mbean: "java.lang:type=Memory", attribute: "HeapMemoryUsage" , path:"used"},
+ *  { type: "read", mbean: "java.lang:type=Memory", attribute: "HeapMemoryUsage" , path:"initial" },
+ *  { type: "read", mbean: "java.lang:type=Memory", attribute: "HeapMemoryUsage" , path:"committed" }
+ *   ], {type: 'logarithmic'} );
+ * 
+ * Parameters
+ * @title Chart title to display
+ * @url URL that Jolokia agent is broadcasting on
+ * @interval number of milliseconds to poll Jolokia
+ * @renderTo CSS selector in which to pin the rendering of the graph to
+ * @seriesType Graph type
+ * @seriesLength number of graph points to retain
+ * @requests Array of Jolokia Requests (See https://jolokia.org/reference/html/clients.html#client-javascript Section 8.2.8.1 on Bulk requests)
+ * @yAxisConfig (optional) To override Y axis with things like logrithmic, or minorTickInterval settings
+ * 
+ * */
+
+function jolokiaMultiSeriesChart(title, url, interval, renderTo, seriesType, seriesLength, requests, yAxisConfig) {
+  var jolokia = new Jolokia(url);
+  var allSeries=[];
+  requests.forEach(function (req){
+    allSeries.push(
+      {data:[], name:req.attribute + (req.path ?":"+req.path:"")}
+    );
+  });
+	
+ var defaultYAxisConfig={title:title};
+ if(!yAxisConfig) yAxisConfig=defaultYAxisConfig;
+
+ return new Highcharts.Chart({
+  chart: {
+   renderTo: renderTo,
+   defaultSeriesType: seriesType,
+   events: {
+    load: function() {
+     var seriesArray=this.series;
+     setInterval(function(){
+       // Send bulk requests 
+       var responses=jolokia.request(requests); 
+       for(var i=0; i<responses.length;++i){
+         var r=responses[i];
+         var point={x:r.timestamp,y:r.value};
+         seriesArray[i].addPoint(
+           point, true, seriesArray[i].data.length >= seriesLength);
+         }
+      }, interval);
+  }
   }
  },
  title: {
-  text: attrName
+  text: title
  },
  xAxis: {
   type: 'datetime'
  },
- yAxis: {
-  title: { text: attrName }
- },
- series: [{
-  data: [],
-  name: attrName + ":" + path
- }]
+  yAxis: yAxisConfig,
+ series: allSeries
 });
 }
